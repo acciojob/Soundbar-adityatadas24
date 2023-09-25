0a235f510a16533e35dcebba7b9837e805cc3e63
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-25 19:59:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your JS code here. If required.
-tdyghjk
+//your JS code here. If required
+const soundsDirectory = 'sounds/';
+let audio = null;
+
+function playSound(soundFileName) {
+    stopSound();
+    audio = new Audio(soundsDirectory + soundFileName);
+    audio.play();
+}
+
+function stopSound() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
