d2ea89be07e1ee2c8ba126afe923f28fb6888fd2
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-10-01 14:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 //your JS code here. If required
-const soundsDirectory = 'sounds/';
-let audio = null;
 
-function playSound(soundFileName) {
-    stopSound();
-    audio = new Audio(soundsDirectory + soundFileName);
-    audio.play();
+function applause1 () {
+	const audio = document.createElement("audio");
+	audio.src = "http://commondatastorage.googleapis.com/codeskulptor-assets/Collision8-Bit.ogg"
+	audio.play();
 }
+function boo () {
+	const audio = document.createElement("audio");
+	audio.src = "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-bounce.m4a"
+	audio.play();
+}
+function gasp () {
+	const audio = document.createElement("audio");
+	audio.src = "http://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg"
+	audio.play();
+}
+function tada () {
+	const audio = document.createElement("audio");
+	audio.src = "http://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg"
+	audio.play();
+}
+function victory () {
+	const audio = document.createElement("audio");
+	audio.src = "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-button.m4a"
+	audio.play();
+}
+function wrong () {
+	const audio = document.createElement("audio");
+	audio.src = "http://commondatastorage.googleapis.com/codeskulptor-assets/jump.ogg"
+	audio.play();
+}
+function stopSound () {
+	const stoped = document.getElementByClassName("btn");
 
-function stopSound() {
-    if (audio) {
-        audio.pause();
-        audio.currentTime = 0;
-    }
+	stoped.stop();
 }
